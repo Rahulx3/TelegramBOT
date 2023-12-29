e72a1dd295f638567041cc6fa770d00b787159a7
Rahulx3 <rahulsajwan98@gmail.com>
2023-12-29 10:01:46 +0530

    added bitcoin api

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
 const{Telegraf} = require('telegraf');
-fetch()
-let bitcoin = "20";
-const bot = new Telegraf('');
+const bot = new Telegraf(process.env.WEATHER_API_KEY);
+
 //ctx is a object passed in the callback function
-bot.start((ctx)=> ctx.reply('welcome from rx'));
-bot.command('bitcoin',(ctx)=> ctx.reply(bitcoin));
+bot.start((ctx)=> ctx.reply('Welcome To Live bitcoin Tracking API'));
 bot.command('whomadethis', (ctx) => ctx.reply('rahul'));
+bot.command('bitcoin', async function(ctx){
+const res = await fetch("https://api.coindesk.com/v1/bpi/currentprice.json");
+const data = await res.json();
+let price = data.bpi.USD.rate; 
+ctx.reply(price)});
 //for running the server continously
 bot.launch();
